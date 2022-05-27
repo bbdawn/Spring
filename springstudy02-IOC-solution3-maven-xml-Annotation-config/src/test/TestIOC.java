461ca5b548b778cf56ad12a9fc088ab0d7043cff
Joohyun Lee <leejoohyun_my@naver.com>
2022-05-28 00:00:54 +0900

    TestIOC

--- springstudy02-IOC-solution3-maven-xml-Annotation-config/src/test/TestIOC.java
+++ springstudy02-IOC-solution3-maven-xml-Annotation-config/src/test/TestIOC.java
@@ -6,6 +6,6 @@ public class TestIOC {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
 		System.out.println(ctx.getBean("tool"));
-		ctx.close();
+		ctx.close(); 
 	}
 }
