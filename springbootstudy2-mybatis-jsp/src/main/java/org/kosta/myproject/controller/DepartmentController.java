26fbce9bafa0dac8d15e5853c026cae5a4d9ee70
Joohyun Lee <leejoohyun_my@naver.com>
2022-06-18 18:53:47 +0900

    DepartmentController.java

---
+++ springbootstudy2-mybatis-jsp/src/main/java/org/kosta/myproject/controller/DepartmentController.java
@@ -0,0 +1,26 @@
+package org.kosta.myproject.controller;
+
+import org.kosta.myproject.mapper.DepartmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DepartmentController {
+	private DepartmentMapper departmentMapper;
+	@Autowired
+	public DepartmentController(DepartmentMapper departmentMapper) {
+		super();
+		this.departmentMapper = departmentMapper;
+	}
+	@RequestMapping("getAllDepartmentCount")
+	public String getAllDepartmentCount(Model model) {
+		model.addAttribute("totalDepartmentCount", departmentMapper.getAllDepartmentCount());
+		return "department-count";
+	}
+}
+
+
+
+
