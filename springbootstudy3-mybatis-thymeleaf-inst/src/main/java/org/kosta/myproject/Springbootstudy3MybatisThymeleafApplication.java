a9669d1459f77eac8220541f7774466f5f599796
Joohyun Lee <leejoohyun_my@naver.com>
2022-06-18 19:05:23 +0900

    Springbootstudy3MybatisThymeleafApplication

---
+++ springbootstudy3-mybatis-thymeleaf-inst/src/main/java/org/kosta/myproject/Springbootstudy3MybatisThymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootstudy3MybatisThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootstudy3MybatisThymeleafApplication.class, args);
+	}
+
+}
