493161a4e0daba42243fb0a3e958c5fafe2db1fa
Joohyun Lee <leejoohyun_my@naver.com>
2022-06-22 22:43:55 +0900

    Springbootstudy7 ajax mybatis dynamic sql application

---
+++ springbootstudy7-ajax-mybatis-inst/src/main/java/org/kosta/myproject/Springbootstudy7AjaxMybatisDynamicSqlApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootstudy7AjaxMybatisDynamicSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootstudy7AjaxMybatisDynamicSqlApplication.class, args);
+	}
+
+}
