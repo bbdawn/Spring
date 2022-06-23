1f33b704d1d5defb35b62e25eba2b849a328747b
Joohyun Lee <leejoohyun_my@naver.com>
2022-06-23 22:13:14 +0900

    department

---
+++ springbootstudy7-ajax-mybatis-resultmap-dynamicsql/src/main/java/org/kosta/myproject/vo/DepartmentVO.java
@@ -0,0 +1,10 @@
+package org.kosta.myproject.vo;
+
+import lombok.Data;
+
+@Data
+public class DepartmentVO {
+	private int departmentNo;
+	private String departmentName;
+	private String location;
+}
