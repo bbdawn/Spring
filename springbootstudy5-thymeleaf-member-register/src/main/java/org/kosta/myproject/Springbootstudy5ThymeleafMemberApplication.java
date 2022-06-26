c3ea5df7f3ce77cc663068720862a04049100df1
Joohyun Lee <leejoohyun_my@naver.com>
2022-06-26 23:56:10 +0900

    Springbootstudy5-Thymeleaf-Member

---
+++ springbootstudy5-thymeleaf-member-register/src/main/java/org/kosta/myproject/Springbootstudy5ThymeleafMemberApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootstudy5ThymeleafMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootstudy5ThymeleafMemberApplication.class, args);
+	}
+
+}
---
+++ springbootstudy5-thymeleaf-member-register/src/test/java/org/kosta/myproject/test/Springbootstudy5ThymeleafMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootstudy5ThymeleafMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
